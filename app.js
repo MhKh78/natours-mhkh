92e5386d5bf29f793dae9180acc45dd88c13b304
MhKh78 <mhkh431@gmail.com>
2019-07-31 21:28:17 +0430

    Added a comment

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
 const viewRouter = require('./routes/viewRoutes');
 
+// Start Express App
 const app = express();
 
 app.set('view engine', 'pug');
